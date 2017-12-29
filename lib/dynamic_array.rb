465e69641ab54f0446ad09276792d039d13d374c
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-29 10:43:25 -0800

    Completes static array and starts dynamic array

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -4,23 +4,41 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @capacity = 4
+    @length = 0
+    @store = StaticArray.new(@capacity)
   end
 
   # O(1)
   def [](index)
+    if index>=@length
+      raise "index out of bounds"
+    else 
+      @store[index]
+    end
   end
 
   # O(1)
   def []=(index, value)
+    if index>@length
+      raise "index out of bounds"
+    else 
+      @store[index] = value
+    end
   end
 
   # O(1)
   def pop
+    res = self[@length-1]
+    @length -= 1 
+    res 
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    self.[]=(@length, val)
+    @length += 1 
   end
 
   # O(n): has to shift over all the elements.
